72a2cae79722efeca5939cedbe89666f8ca962ec
seacat271 <seacat271083@gmail.com>
2022-09-14 09:21:50 +0300

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+import {Component} from "react";
+
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: ''
+  }
+
+  render () {
+    return <div>
+<h1>Phonebook</h1>
+<form>
+  <label>
+  <input
+      type="text"
+      name="name"
+      pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+      title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+      required
+  />
+  </label>
+  <button type="submit">Add contact</button>
+</form>
+<h2>Contacts</h2>
+<ul>
+  <li>Rose</li>
+  <li>Rose</li>
+  <li>Rose</li>
+</ul>
     </div>
-  );
-};
+  }
+}
+
+
+
+
+
+
+
+
+// export const App = () => {
+//   return (
+//     <div>
+// <h1>Phonebook</h1>
+// <form>
+//   <label>
+//     <input></input>
+//   </label>
+//   <button type="submit">Add contact</button>
+// </form>
+// <h2>Contacts</h2>
+// <ul>
+//   <li>Rose</li>
+//   <li>Rose</li>
+//   <li>Rose</li>
+// </ul>
+//     </div>
+//   );
+// };
